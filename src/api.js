51d1d7aa9bb699e24bed421c6630dbb1192f91c7
Nestor-eng <70307354+Nestor-eng@users.noreply.github.com>
2026-05-07 01:16:11 -0600

    Update API base URL to use environment variable

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const api=axios.create({baseURL:'https://laspromosxoxo.com/api'})
+export const api=axios.create({baseURL: import.meta.env.VITE_API_URL})
 api.interceptors.request.use(c=>{const t=localStorage.getItem('xoxo_token');if(t)c.headers.Authorization=`Bearer ${t}`;return c})
 export const money=v=>new Intl.NumberFormat('es-MX',{style:'currency',currency:'MXN'}).format(Number(v||0))
 export const user=()=>{try{return JSON.parse(localStorage.getItem('xoxo_user')||'{}')}catch{return{}}}
