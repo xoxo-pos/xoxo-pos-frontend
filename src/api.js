a53efbecc40f620f9462f6d56ffa71ff484317f4
Nestor-eng <70307354+Nestor-eng@users.noreply.github.com>
2026-05-06 23:10:55 -0600

    Change API base URL to production server

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-export const api=axios.create({baseURL:'http://localhost:8080/api'})
+export const api=axios.create({baseURL:'https://laspromosxoxo.com/api'})
 api.interceptors.request.use(c=>{const t=localStorage.getItem('xoxo_token');if(t)c.headers.Authorization=`Bearer ${t}`;return c})
 export const money=v=>new Intl.NumberFormat('es-MX',{style:'currency',currency:'MXN'}).format(Number(v||0))
 export const user=()=>{try{return JSON.parse(localStorage.getItem('xoxo_user')||'{}')}catch{return{}}}
